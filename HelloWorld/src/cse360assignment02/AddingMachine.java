b05d878415e0262c3dd9bb4769e6297e0f262658
Frankincense Ramesh <vmrfrankie@gmail.com>
2020-10-07 21:36:02 -0700

    Added javac comments

--- HelloWorld/src/cse360assignment02/AddingMachine.java
+++ HelloWorld/src/cse360assignment02/AddingMachine.java
@@ -4,23 +4,25 @@ public class AddingMachine {
   //private int total;
   
   public AddingMachine () {
-   // total = 0;  // not needed - included for clarity
+   //total = 0;  // not needed - included for clarity
   }
   
-  public int getTotal () {
-    return 0;
+  public int getTotal () {/**returns the current total*/
+   return 0;
   }
   
-  public void add (int value) {
+  public void add (int value) {/**the add method adds the parameter to the total variable*/
+  
   }
 
-  public void subtract (int value) {
+  public void subtract (int value) {/**the subtract method subtracts the parameter from the total variable*/
+  
   }
 
-  public String toString () {
+  public String toString () {/**history of the transactions returned to this method*/
     return "";
   }
 
-  public void clear() {
+  public void clear() {/**clears AddingMachine*/
   }
 }
